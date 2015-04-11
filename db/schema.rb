155354fa00d5e0b167ad658f6f5047ab77bbd2f7
Kelvin Smith <oneKelvinSmith@gmail.com>
2015-04-11 20:50:31 +0530

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,25 +1,27 @@
 require 'sequel'
+require 'logger'
 
 module Tickets
   module Database
-    DB = Sequel.connect('postgres://postgres:pass@localhost:5432/tickets')
+    DB = Sequel.connect('postgres://postgres:pass@localhost:5432/tickets',
+                        :logger => Logger.new('log/db.log'))
 
     def Database.init
-      DB.create_table :order_seats do
-        primary_key :id
-        String :booked_seat
-        Integer :session_id
-        unique [:session_id, :booked_seat]
-      end
-
       DB.create_table :sessions do
         primary_key :id
         String :name
       end
 
-      DB.create_table :orders do
+      DB.create_table :order_seats do
         primary_key :id
+        String :booked_seat
+        foreign_key :session_id, :sessions, :unique=>true, :null=>false
+        unique [:session_id, :booked_seat]
+      end
+
+      DB.create_table :orders do
         Date :create_at
+        String :order_id, :primary_key => true
       end
     end
   end
